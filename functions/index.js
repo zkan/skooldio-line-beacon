96c1b5acf175a2e676064e59ad24d81623a332e7
zkan <zkan.cs@gmail.com>
2022-09-08 15:37:32 +0700

    Set up project baseline

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,37 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.myWebhook = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+// 0. npm install axios
+// 1. Import template and util
+const template = require("./template")
+const util = require("./util")
+// 2. Fill in your CHANNEL ACCESS TOKEN in util.js
+
+exports.myWebhook = functions.https.onRequest(async (request, response) => {
+  const events = request.body.events;
+
+  for (const event of events) {
+    // 3. Detect beacon event
+    if (event) {
+      // 4. Log body payload
+
+      // 5. Get userId
+      
+      // 6. Get user profile
+      
+      // 7. Detect beacon type
+      switch (event) {
+        // 8. If beacon type is enter then reply with Flex message
+        // 9. Replying personal messages separated by each device
+
+        // 10. Create a Rich Menu then fill in your RICH MENU ID in util.js
+        // 11. If beacon type is banner then link a personal Rich Menu to user
+
+        // 12. If beacon type is stay then write user profile to database
+      }
+    }
+  }
+
+  response.end();
 });
